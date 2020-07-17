56ed3076c14b56d376a4b30328b9ede95bdf1777
Eduards Birznieks <eduards.latvia@gmail.com>
2020-07-17 16:18:42 +0200

    first comint

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+import {Link} from "gatsby"
+import { RiHeart2Line } from "react-icons/ri";
+
+const Footer = () => (
+  <footer className="site-footer">
+    <div className="container">
+      <p>A Tore Guldbrandsen portfolio, Made by <Link to="/">http://eddythedev.com/</Link></p>
+    </div>
+  </footer>
+)
+
+export default Footer
